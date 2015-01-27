03f977d21d7b31b55ee0120d707dd976ff4ce3b9
Mike <mike@digitalwebmedia.co.uk>
2015-01-27 20:50:12 +0000

    new change

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -5,5 +5,27 @@
 <title>Keto Gym Web App</title>
 
 <!-- load bootstrap from a cdn -->
-<link rel="stylesheet" href="{{ URL::asset('css/default.css') }}" type="text/css"> 
+<link rel="stylesheet" href="{{ URL::asset('css/global/components-rounded.css') }}" type="text/css" ="style_components"> 
+<link rel="stylesheet" href="{{ URL::asset('css/global/plugins.css') }}" type="text/css"> 
+<link rel="stylesheet" href="{{ URL::asset('css/global/components.css') }}" type="text/css"> 
+
+<link rel="stylesheet" href="{{ URL::asset('plugins/font-awesome/css/font-awesome.min.css') }}" type="text/css"> 
+<link rel="stylesheet" href="{{ URL::asset('plugins/simple-line-icons/simple-line-icons.min.css') }}" type="text/css"> 
+<link rel="stylesheet" href="{{ URL::asset('plugins/bootstrap/css/bootstrap.min.css') }}" type="text/css"> 
+<link rel="stylesheet" href="{{ URL::asset('plugins/uniform/css/uniform.default.css') }}" type="text/css"> 
+
+
+
+<link rel="stylesheet" href="{{ URL::asset('css/layout.css') }}" type="text/css"> 
 <link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}" type="text/css"> 
+<link rel="stylesheet" href="{{ URL::asset('css/default.css') }}" type="text/css" id="style_color"/> 
+
+<link rel="stylesheet" href="{{ URL::asset('css/pages/login.css') }}" type="text/css"> 
+
+
+
+
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+
+
+
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,55 +4,129 @@
 <html>
 <head>
 	<title>Look at me Login</title>
-	<link rel="stylesheet" href="{{ URL::asset('css/default.css') }}" type="text/css"> 
+	    @include('includes.head')
 </head>
 <body>
 
-
-
-
 <body class="login">
-    <div id="content">
-        <div class="container-fluid">
-            <div class="lock-container">
-                <h1>Account Access</h1>
-                <div class="panel panel-default text-center">
-                   
-                    <div class="panel-body">
-                        {{ Form::open(array('url' => 'login')) }}
-		<h1>Login</h1>
 
-		<!-- if there are login errors, show them here -->
-		<p>
+<div class="content">
+	<!-- BEGIN LOGIN FORM -->
+		{{ Form::open(array('url' => 'login', 'class'=>'login-form' )) }}
+		<h3 class="form-title">Sign In</h3>
+		<div class="alert alert-danger display-hide">
+			<button class="close" data-close="alert"></button>
+			<span>
+			Enter any username and password. </span>
+		</div>
 			{{ $errors->first('email') }}
 			{{ $errors->first('password') }}
-		</p>
+		
+		<h1>Login</h1>
+			<div class="form-group">
+			{{ Form::label('email', 'Email Address', array('class' =>'control-label visible-ie8 visible-ie9')) }}
+			{{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com', 'class' => 'form-control form-control-solid placeholder-no-fix')) }}
+			<div>
 
+			<div class="form-group">
+			{{ Form::label('password', 'Password', array('class' =>'control-label visible-ie8 visible-ie9')) }}
+			{{ Form::password('password', array( 'class' => 'form-control form-control-solid placeholder-no-fix')) }}
+			</div>
+
+		
+	
+		<div class="form-actions">
+		{{ Form::submit('Log In', array( 'class' => 'btn btn-success uppercase')) }}
+			
+			<label class="rememberme check">
+			<input type="checkbox" name="remember" value="1"/>Remember </label>
+			<a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
+		</div>
+		
+		<div class="create-account">
+			<p>
+				<a href="javascript:;" id="register-btn" class="uppercase">Create an account</a>
+			</p>
+		</div>
+		{{ Form::close() }} 
+	</div>
+	<!-- END LOGIN FORM -->
+	<!-- BEGIN FORGOT PASSWORD FORM -->
+	<form class="forget-form" action="index.html" method="post">
+		<h3>Forget Password ?</h3>
 		<p>
-			{{ Form::label('email', 'Email Address') }}
-			{{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com', 'class' => 'form-control')) }}
+			 Enter your e-mail address below to reset your password.
 		</p>
-
-		<p>
-			{{ Form::label('password', 'Password') }}
-			{{ Form::password('password', array( 'class' => 'form-control')) }}
+		<div class="form-group">
+			<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email"/>
+		</div>
+		<div class="form-actions">
+			<button type="button" id="back-btn" class="btn btn-default">Back</button>
+			<button type="submit" class="btn btn-success uppercase pull-right">Submit</button>
+		</div>
+	</form>
+	<!-- END FORGOT PASSWORD FORM -->
+	<!-- BEGIN REGISTRATION FORM -->
+	<form class="register-form" action="index.html" method="post">
+		<h3>Sign Up</h3>
+		<p class="hint">
+			 Enter your personal details below:
 		</p>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Full Name</label>
+			<input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname"/>
+		</div>
+		<div class="form-group">
+			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+			<label class="control-label visible-ie8 visible-ie9">Email</label>
+			<input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Address</label>
+			<input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">City/Town</label>
+			<input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city"/>
+		</div>
+		
+		<p class="hint">
+			 Enter your account details below:
+		</p>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Username</label>
+			<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Password</label>
+			<input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
+			<input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="rpassword"/>
+		</div>
+		<div class="form-group margin-top-20 margin-bottom-20">
+			<label class="check">
+			<input type="checkbox" name="tnc"/> I agree to the <a href="#">
+			Terms of Service </a>
+			& <a href="#">
+			Privacy Policy </a>
+			</label>
+			<div id="register_tnc_error">
+			</div>
+		</div>
+		<div class="form-actions">
+			<button type="button" id="register-back-btn" class="btn btn-default">Back</button>
+			<button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit</button>
+		</div>
+	</form>
+	<!-- END REGISTRATION FORM -->
+</div>
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
 
-		<p>{{ Form::submit('Submit!', array( 'class' => 'btn btn-primary')) }}</p>
-	{{ Form::close() }} 
 
-                        <a href="#" class="forgot-password">Forgot password?</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
 
-<!-- Vendor Scripts Bundle -->
-<script src="js/vendor.min.js"></script>
 
-<!-- App Scripts Bundle -->
-<script src="js/scripts.min.js"></script>
-</body>
-</html>
+
