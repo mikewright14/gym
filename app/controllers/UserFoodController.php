a49592d756dfc8b6924ed83ac9c464dc636c7326
Mike Wright <mike@digitalwebmedia.co.uk>
2015-01-28 08:22:04 +0000

    the new look

--- app/controllers/UserFoodController.php
+++ app/controllers/UserFoodController.php
@@ -1,34 +1,15 @@
 <?php
 
-class UserFoodController extends \BaseController {
-
-
+class userFoodController extends \BaseController {
 
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
-	public function view($date)
+	public function index()
 	{
-		$id = Auth::id();
-		
-	  
-   
-
-		$users = DB::select( DB::raw("SELECT * FROM users
-		LEFT JOIN user_food
-		ON `users`.`id` = `user_food`.`user_id` 
-		LEFT JOIN food
-		ON `user_food`.`food_id` = `food`.`foodID`
-		WHERE `user_food`.`user_id` = :id AND `user_food`.`dateEntry` = :mealDate"), array(
-   		'id' => $id, 'mealDate' => $date, 
- 		));
-
- 		$prev =  date('Y-m-d',(strtotime ( '-1 day' , strtotime ( $date) ) ));
- 		$next =  date('Y-m-d',(strtotime ( '+1 day' , strtotime ( $date) ) ));
-	
-		return View::make('UserFood.view')->with('users', $users)->with('date', $date)->with('next', $next)->with('prev', $prev);
+		//
 	}
 
 
@@ -37,40 +18,10 @@ class UserFoodController extends \BaseController {
 	 *
 	 * @return Response
 	 */
-	public function create($date, $mealtype)
+	public function create()
 	{
-		$id = Auth::id();
-		$users = User::find($id)->UserFood;
-
-
-		$users = DB::select(DB::raw("SELECT * FROM `users`
-							LEFT JOIN `user_food` 
-							ON `users`.`id` = `user_food`.`user_id`
-							LEFT JOIN `food`
-							ON `user_food`.`food_id` = `food`.`foodID` "));
-
-
-		$recents = DB::select(DB::raw("SELECT * FROM `user_food`
-							LEFT JOIN `food` 
-							ON `user_food`.`food_id` = `food`.`foodID`
-							GROUP by `user_food`.`food_id`"));
-
-		$frequents = DB::select(DB::raw("SELECT *,COUNT(*) FROM `user_food`
-							LEFT JOIN `food` 
-							ON `user_food`.`food_id` = `food`.`foodID`
-							GROUP by `user_food`.`food_id`
-							ORDER by count(*) DESC"));
-
-
-			
-
-
-		
-
-		return View::make('UserFood.create')->with('users', $users)->with('recents', $recents)->with('frequents', $frequents)->with('date', $date)->with('mealtype', $mealtype);
-
+		return View::make('userFood.create');
 	}
-
 
 
 	/**
@@ -79,45 +30,8 @@ class UserFoodController extends \BaseController {
 	 * @return Response
 	 */
 	public function store()
-	{	
-		
-
-	$food_checked = Input::get('foodID');
-	$foodAmount_checked = Input::get('foodWeight');
-	$id = Auth::id();
-
-	$test = array_diff($food_checked, $foodAmount_checked);
-	$keySelected = array();
-	foreach($test as $selected){
-		$keySelected[] = $selected;
-	}
-	
-	if(is_array($food_checked))
 	{
-		
-   		foreach($food_checked as $key => $foodID){
-		if (in_array($key, $keySelected)){
-			
-		
-
-		$foodAmount = $foodAmount_checked[$key];
-   		$food = new UserFood;
-		$food->user_id = $id;
-	    $food->food_id = $foodID;
-	    $food->mealType = Input::get('mealType');
-	    $food->foodAmount = $foodAmount_checked[$key];
-	    $food->dateEntry = Input::get('date');
-	    $food->save();
-
-	    }
-
-   		}
-	}
-		
-		
-
-		
-    	return Redirect::to('user/food/'.Input::get('date'));
+		//
 	}
 
 
--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,11 +1,45 @@
-        <div class="footer">Mike Wright 2014</div>
-    </div>
 
-    <!-- Vendor Scripts Bundle -->
-    <script src="js/vendor.min.js"></script>
-    
+<?php Header("Content-Type: application/x-javascript; charset=UTF-8"); ?>
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="../../assets/global/plugins/respond.min.js"></script>
+<script src="../../assets/global/plugins/excanvas.min.js"></script> 
+<![endif]-->
 
-    <!-- App Scripts Bundle -->
-    <script src="js/scripts.min.js"></script>
+
+{{ HTML::script('plugins/jquery.min.js') }}
+{{ HTML::script('plugins/jquery-migrate.min.js') }}
+{{ HTML::script('plugins/bootstrap/js/bootstrap.min.js') }}
+{{ HTML::script('plugins/jquery.blockui.min.js') }}
+{{ HTML::script('plugins/uniform/jquery.uniform.min.js') }}
+{{ HTML::script('plugins/jquery.cokie.min.js') }}
+{{ HTML::script('global/scripts/metronic.js') }}
+{{ HTML::script('layout/scripts/layout.js') }}
+{{ HTML::script('layout/scripts/demo.js') }}
+{{ HTML::script('pages/scripts/login.js') }}
+{{ HTML::script('plugins/jquery-validation/js/jquery.validate.min.js') }}
+
+
+
+
+
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+
+<!-- END PAGE LEVEL SCRIPTS -->
+<script>
+jQuery(document).ready(function() {     
+Metronic.init(); // init metronic core components
+Layout.init(); // init current layout
+Login.init();
+Demo.init();
+});
+</script>
+<!-- END JAVASCRIPTS -->
 </body>
+<!-- END BODY -->
 </html>
